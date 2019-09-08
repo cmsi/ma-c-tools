35a2521aeb28e9f3605e051026844eb575ae2d73
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2019-09-08 10:29:51 +0900

    Remove -sa

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p $TARGET_DIR
 cd $BUILD_DIR 
 case "$(dpkg --print-architecture)" in
     amd64)
-        dpkg-buildpackage -us -uc -sa
+        dpkg-buildpackage -us -uc
         awk '$3!="debug" {print}' ../${PACKAGE}_${VERSION}_amd64.changes > $TARGET_DIR/${PACKAGE}_${VERSION}_amd64.changes
         mv -f ../${PACKAGE}*_${VERSION_BASE}*.buildinfo ../${PACKAGE}*_${VERSION_BASE}*.deb $TARGET_DIR
         mv -f ../${PACKAGE}*_${VERSION_BASE}*.dsc ../${PACKAGE}*_${VERSION_BASE}*.debian.tar.* $TARGET_DIR
